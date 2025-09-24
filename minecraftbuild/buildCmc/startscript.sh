87018a3247e4b7238687e9eb7154b2a9e8b5e13c
UmaruDoma2 <schreiber@screenpaper.de>
2025-09-24 22:17:54 +0200

    build21.8

--- minecraftbuild/buildCmc/startscript.sh
+++ minecraftbuild/buildCmc/startscript.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #######################################################
 # Create Minecraft Server File
-# docker run --rm  -v $(pwd)/output:/data buildCmc
+# docker run --rm  -v $(pwd)/output:/data build_c_mc
 # (c) 2022 cndrbrbr
 #######################################################
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar \
