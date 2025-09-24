ada6fbea6caf69faff5f2ef0e661f9f2e1fc37b2
UmaruDoma2 <schreiber@screenpaper.de>
2025-09-24 20:41:46 +0200

    newJava8

--- minecraftbuild/build8mc/startscript.sh
+++ minecraftbuild/build8mc/startscript.sh
@@ -2,12 +2,15 @@
 #!/bin/bash
 #######################################################
 # Create Minecraft Server File
-# docker build -t build14.4mc .
+# docker run --rm  -v $(pwd)/output:/data build8mc  sh -c "echo 'Hallo Welt3' > /data/ergebnis2.txt"
 #
 # (c) 2022 cndrbrbr
 #######################################################
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
-sleep 5
-java -jar BuildTools.jar --rev 1.14.4 
-cp /root/spigot*.jar /root/updates/
-sleep infinity 
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar \
+    && java -Xmx1024M -jar BuildTools.jar --rev 1.14.4 
+
+wget https://github.com/CreeperHost/Log4jPatcher/releases/download/v1.0.1/Log4jPatcher-1.0.1.jar
+
+mv spigot-*.jar /data
+mv Log4jPatcher*.jar /data
+
---
+++ minecraftbuild/buildCmc/startscript.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+#######################################################
+# Create Minecraft Server File
+# docker run --rm  -v $(pwd)/output:/data buildCmc
+# (c) 2022 cndrbrbr
+#######################################################
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar \
+    && java -Xmx1024M -jar BuildTools.jar --rev 1.21.8
+
+mv spigot-*.jar /data
