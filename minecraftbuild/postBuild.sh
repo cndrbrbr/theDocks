480a9f580846f94d584262683065c13d0f76dad6
UmaruDoma2 <schreiber@screenpaper.de>
2025-09-24 22:34:57 +0200

    copyjars

---
+++ minecraftbuild/postBuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#######################################################
+# Copy  Minecraft Server Files
+# 
+# (c) 2025 cndrbrbr
+#######################################################
+cp ./build8mc/output/spigot-1.14.4.jar ../minecraft/java8mc/
+cp ./buildCmc/output/spigot-1.21.8.jar ../minecraft/java22mc/
