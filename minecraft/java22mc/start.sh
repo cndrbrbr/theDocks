814099b035c5451a6a55c5291676b5d8fb8df215
cndrbrbr <info@screenpaper.de>
2025-12-07 14:27:33 +0100

    mcbuild25 und java22mc funktionieren

--- minecraft/java22mc/start.sh
+++ minecraft/java22mc/start.sh
@@ -12,29 +12,31 @@
 servername="nc3"
 levelname="world"
 maxplayers=5
-spigot="spigot.1.21.10.jar"
-host="192.168.1.62"
+spigot="spigot-1.21.10.jar"
+#host="localhost"
 port=25566
+	 #-jar $spigot \   	--initSettings \
 
+cd /root/mcsrvbase
+echo "eula=true" > eula.txt
 while true; do
     echo "Starte Minecraft..."
 
-	java -Xms2G -Xmx4G \
-	 -Dcom.mojang.eula.agree=true \
-	 -Dfile.encoding=UTF-8 \
-	 -XX:StartFlightRecording=filename=./data/jfr/minecraft.jfr,dumponexit=true \
-	 -jar $spigot \
-	 --bukkit-settings ./data/cfg/bukkit.yml \
-	 --spigot-settings ./data/cfg/spigot.yml \
-	 --commands-settings ./data/cfg/commands.yml \
-	 --config ./data/cfg/server.properties \
-	 --host $host --port $port \
-	 --level-name $levelname --max-players $maxplayers \
-	 --online-mode true \
-	 --plugins ./data/plugins \
-	 --serverId $servername \
-	 --world-dir ./data/worlds \
-	 nogui 
+	java -Xms1G -Xmx1G \
+	-Dfile.encoding=UTF-8 \
+	-jar $spigot \
+	--bukkit-settings "./data/cfg/bukkit.yml" \
+	--spigot-settings "./data/cfg/spigot.yml" \
+	--commands-settings "./data/cfg/commands.yml" \
+	--config "./data/cfg/server.properties" \
+	--port "$port" \
+	--level-name "$levelname" \
+	--max-players "$maxplayers" \
+	--online-mode "true" \
+	--plugins "./data/plugins" \
+	--serverId "$servername" \
+	--world-dir "./data/worlds" \
+	nogui
 
 	EXIT_CODE=$?
     echo "Server gestoppt mit Code $EXIT_CODE"
@@ -49,3 +51,6 @@ while true; do
     echo "Crash erkannt - Neustart in 5 Sekunden..."
     sleep 5
 done
+
+
+#	#--host "$host" \
